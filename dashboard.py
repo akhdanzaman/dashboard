277c27bbb12d3b37774bc273c4ca0960f73a19fd
akhdanzaman <akhdanzaman@gmail.com>
2024-03-05 18:30:23 +0700

    Ver 3

--- dashboard.py
+++ dashboard.py
@@ -12,24 +12,6 @@ def read_file(file):
 def main():
     st.title("Aplikasi Analisis Data Bike Sharing")
     
-    # Upload file hour.csv
-    st.subheader("Upload file hour.csv")
-    uploaded_hour_file = st.file_uploader("Upload CSV file for hour data", type=["csv"])
-    if uploaded_hour_file is not None:
-        hour_df = read_file(uploaded_hour_file)
-        st.write("Data hour.csv berhasil dimuat.")
-        
-        # Menjawab pertanyaan 1
-        st.subheader("Pertanyaan 1: Bagaimana pengaruh suhu terhadap jumlah peminjam sepeda?")
-        st.write("Visualisasi hubungan antara suhu dan jumlah peminjam sepeda:")
-        sns.set_style("whitegrid")
-        fig, ax = plt.subplots(figsize=(12, 8))
-        sns.scatterplot(x=hour_df['temp'], y='cnt' ,data=hour_df, palette='viridis', alpha=0.7, ax=ax)
-        ax.set_title('Hubungan antara Suhu dan Jumlah Penyewa Sepeda', fontsize=16)
-        ax.set_xlabel('Suhu (Celcius)', fontsize=14)
-        ax.set_ylabel('Jumlah Penyewa', fontsize=14)
-        st.pyplot(fig)
-
     # Upload file day.csv
     st.subheader("Upload file day.csv")
     uploaded_day_file = st.file_uploader("Upload CSV file for day data", type=["csv"])
@@ -37,12 +19,30 @@ def main():
         day_df = read_file(uploaded_day_file)
         st.write("Data day.csv berhasil dimuat.")
         
+        # Menjawab pertanyaan 1
+        st.subheader("Pertanyaan 1: Bagaimana pengaruh suhu terhadap jumlah peminjam sepeda?")
+        st.write("Visualisasi hubungan antara suhu dan jumlah peminjam sepeda:")
+        sns.set_style("whitegrid")
+        fig, ax = plt.subplots(figsize=(12, 8))
+        sns.scatterplot(x=day_df['temp'], y='cnt' ,data=day_df, palette='viridis', alpha=0.7, ax=ax)
+        ax.set_title('Hubungan antara Suhu dan Jumlah Penyewa Sepeda', fontsize=16)
+        ax.set_xlabel('Suhu (Celcius)', fontsize=14)
+        ax.set_ylabel('Jumlah Penyewa', fontsize=14)
+        st.pyplot(fig)
+
+    # Upload file hour.csv
+    st.subheader("Upload file hour.csv")
+    uploaded_hour_file = st.file_uploader("Upload CSV file for hour data", type=["csv"])
+    if uploaded_hour_file is not None:
+        hour_df = read_file(uploaded_hour_file)
+        st.write("Data hour.csv berhasil dimuat.")
+        
         # Menjawab pertanyaan 2
         st.subheader("Pertanyaan 2: Pada jam berapa jumlah peminjam banyak berdatangan?")
         st.write("Visualisasi jumlah peminjam sepeda per jam:")
         sns.set_style("whitegrid")
         fig, ax = plt.subplots(figsize=(12, 8))
-        sns.barplot(y="cnt", x="hr", data=day_df, color="skyblue", ax=ax)
+        sns.barplot(y="cnt", x="hr", data=hour_df, color="skyblue", ax=ax)
         ax.set_title('Jumlah Penyewa Sepeda per Jam', fontsize=16)
         ax.set_xlabel('Jam', fontsize=14)
         ax.set_ylabel('Jumlah Penyewa', fontsize=14)
